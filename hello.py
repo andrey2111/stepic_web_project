cceb6287e16b5a418300927ac79e441b8ef06376
andrey2111 <andrey2111@mail.ru>
2016-03-01 02:25:47 +0300

    initial

---
+++ hello.py
@@ -0,0 +1,10 @@
+def wsgi_application(environ, start_response):
+    body = '\n'.join(i for i in environ.get("QUERY_STRING").split('&'))
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    start_response(status, headers)
+    return [body]
+
+# environ = {"QUERY_STRING": "sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8&client=ubuntu"}
+# body = '\n'.join(i for i in environ.get("QUERY_STRING").split('&'))
+# print(body)
