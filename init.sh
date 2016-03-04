85f35f76789a9238e1d9081e33ca85940920c58a
andrey2111 <andrey2111@mail.ru>
2016-03-05 01:52:25 +0300

    initial

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/hello /etc/gunicorn.d/hello
+sudo ln -sf /home/box/web/etc/ask /etc/gunicorn.d/ask
+
 sudo /etc/init.d/gunicorn restart
