919198e0c0bfe4e9a08a0e9aa3776c8f6bd218f8
andrey2111 <andrey2111@mail.ru>
2016-03-07 20:13:30 +0300

    initial

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,34 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-class Post(models.Model):
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, null=True, related_name='question_author')
+    likes = models.ManyToManyField(User)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'questions'
+        ordering = ['-added_at']
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True)
+
+    def __unicode__(self):
+        return str(self.pk)
+
+    class Meta:
+        db_table = 'answers'
+
--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 sudo mysql -uroot -pandrey -e  "CREATE DATABASE ask_data"
 sudo mysql -uroot -pandrey -e  "CREATE USER 'andrey'@'localhost' IDENTIFIED BY 'andrey'"
 sudo mysql -uroot -pandrey -e  "GRANT ALL PRIVILEGES ON * . * TO 'andrey'@'localhost'"
+python ask/manage.py makemigrations
+python ask/manage.py migrate
 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
