16c71d0d608c8e4f36113651038f2775745dbb2c
andrey2111 <andrey2111@mail.ru>
2016-03-14 03:10:49 +0300

    initial

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import Http404
 from django.http import HttpResponse
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 from models import Question
 
 
@@ -10,13 +9,29 @@ def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
 
+def paginate(request, qs):
+    try:
+        limit = int(request.GET.get('limit', 10))
+    except ValueError:
+        limit = 10
+    if limit > 100:
+        limit = 10
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        raise Http404
+    paginator = Paginator(qs, limit)
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    return paginator, page
+
+
 def main(request):
     questions = Question.objects.order_by('-id')
-    page = request.GET.get('page', 1)
-    limit = request.GET.get('limit', 10)
-    paginator = Paginator(questions, limit)
+    paginator, page = paginate(request, questions)
     paginator.baseurl = '/?page='
-    page = paginator.page(page)
     return render(request, 'main.html', {
         'questions': page.object_list,
         'paginator': paginator,
